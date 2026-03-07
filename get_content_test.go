686be85f4234c193bcab77ad7c758a57444d3c06
rafidoth <rafiulhasan803@gmail.com>
2026-03-07 19:23:03 +0600

    test : TestGetFirstParagraphFromHTML

--- get_content_test.go
+++ get_content_test.go
@@ -12,17 +12,17 @@ func TestGetHeadingFromHTMLBasic(t *testing.T) {
 	}
 }
 
-// func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
-// 	inputBody := `<html><body>
-// 		<p>Outside paragraph.</p>
-// 		<main>
-// 			<p>Main paragraph.</p>
-// 		</main>
-// 	</body></html>`
-// 	actual := getFirstParagraphFromHTML(inputBody)
-// 	expected := "Main paragraph."
-//
-// 	if actual != expected {
-// 		t.Errorf("expected %q, got %q", expected, actual)
-// 	}
-// }
+func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
+	inputBody := `<html><body>
+		<p>Outside paragraph.</p>
+		<main>
+			<p>Main paragraph.</p>
+		</main>
+	</body></html>`
+	actual := getFirstParagraphFromHTML(inputBody)
+	expected := "Main paragraph."
+
+	if actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
