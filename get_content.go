c25e179236665ee2527f512f0ce6caa9968120ab
rafidoth <rafiulhasan803@gmail.com>
2026-03-07 19:22:38 +0600

    add getFirstParagraphFromHTML

--- get_content.go
+++ get_content.go
@@ -22,3 +22,23 @@ func getHeadingFromHTML(rawHTML string) string {
 	}
 	return ""
 }
+
+func getFirstParagraphFromHTML(rawHTML string) string {
+	doc, err := html.Parse(strings.NewReader(rawHTML))
+	if err != nil {
+		panic(err)
+	}
+
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.DataAtom == atom.Main {
+			for n1 := range n.Descendants() {
+				if n1.Type == html.ElementNode && n1.DataAtom == atom.P {
+					if n1.FirstChild != nil && n1.FirstChild.Type == html.TextNode {
+						return n1.FirstChild.Data
+					}
+				}
+			}
+		}
+	}
+	return ""
+}
